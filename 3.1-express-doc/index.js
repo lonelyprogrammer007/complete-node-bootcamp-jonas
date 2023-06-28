72061f747213302895bb9bf4c0043dba527cd0fe
Luis Guillermo Figueroa Hernandez <lugfigue@bancolombia.com.co>
2023-06-28 02:25:45 -0500

    c

--- 3.1-express-doc/index.js
+++ 3.1-express-doc/index.js
@@ -1,23 +1,15 @@
 const express = require('express')
 const app = express()
-const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+// parse incoming JSON payloads
+app.use(express.json())
+
+app.get('/api/data', (req, res) => {
+  console.log(req.body) // This will print the JSON payload to the console
+  throw Error(':(')
+  // res.send('Data received')
 })
 
-app.post('/', (req, res) => {
-  res.send('Got a POST request')
-})
-
-app.put('/user', (req, res) => {
-  res.send('Got a PUT request at /user')
-})
-
-app.delete('/user', (req, res) => {
-  res.send('Got a DELETE request at /user')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000')
 })
---
+++ 4-natours/starter/app.js
@@ -0,0 +1,49 @@
+const fs = require('fs')
+const path = require('path')
+
+const express = require('express')
+
+const app = express()
+
+app.use(express.json())
+
+const tours = JSON.parse(
+  fs.readFileSync(path.join(__dirname, 'dev-data', 'data', 'tours-simple.json'))
+)
+
+app
+  .route('/api/v1/tours')
+  .get((req, res) => {
+    res
+      .status(200)
+      .json({status: 'success', results: tours.length, data: {tours}})
+  })
+  .post((req, res, next) => {
+    const id = tours[tours.length - 1].id + 1
+    const tour = structuredClone(req.body)
+    tour.id = id
+
+    tours.push(tour)
+    fs.writeFile(
+      path.join(__dirname, 'dev-data', 'data', 'tours-simple.json'),
+      JSON.stringify(tours),
+      err => {
+        if (err) return next(err)
+        res.status(201).json({
+          status: 'success',
+          data: {
+            tour
+          }
+        })
+      }
+    )
+  })
+
+app.use((err, req, res, next) => {
+  console.error('💥💥💥💥💥💥', err.stack)
+  res.status(500).send('Something broke!')
+})
+
+const port = 3000
+
+app.listen(port, () => console.log(`App running on port ${port}`))
